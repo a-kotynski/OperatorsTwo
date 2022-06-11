7310a1e74a5f300fb11cb9d767b96a8a1c24a4c0
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-11 13:01:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  
+             *  C# Operators
+                
+             *  Arithmetic Operators    
+             *  Comparison Operators    
+             *  Asssignment Operators   
+             *  Logical Operators       
+             *  Bitwise Operators       
+               
+               Arithmetic Operators
+                        Operators:      Example:    Same as:
+            Add         +               a + b
+            Subtract    -               a - b
+            Multiply    *               a * b
+            Divide      /               a / b
+            Remainder   %               a % b
+            Increment   ++              a++         a = a + 1
+            Decrement   --              a--         a = a - 1
+                
+             */
+
+            /*
+            //Postfix increment
+            int a = 1;
+            int b = a++; // in this line a = 2 and b = 1
+            */
+
+            /*
+            //Prefix increment
+            int a = 1;
+            int b = ++a; // in this line a = 2 and b = 2
+            */
+
+            /*
+            Comparison Operators
+                                        Operator:        Example:    Same as:
+            Equal                       ==               a == b
+            Not Equal                   !=               a != b
+            Greater than                >                a > b
+            Greater than or equal to    >=               a >= b
+            Less than                   <                a < b
+            Less than or equal to       <=               a <= b
+            */
+
+            /*
+            Assignment operators
+                                        Operator:         Example:     Same as:
+            Assigment                   =                 a = 1
+            Addition assignment         +=                a += 3        a = a + 3
+            Subtraction assignment      -=                a -= 3        a = a - 3
+            Multiplication assignment   *=                a *= 3        a = a * 3
+            Division assigment          /=                a /= 3        a = a / 3
+             */
+
+            /*
+            Logical Operators
+                                        Operator:       Example:
+            And                         &&              a&&b
+            Or                          ||              a||b
+            Not                         !               !a
+             */
+
+            /*
+            Bitwise Operators
+                                        Operator:       Example:
+            And                         &               a&b
+            Or                          |               a|b
+             */
+        }
+    }
+}
